18da190b7b75189ad92eccbc5e989a303dff8bdf
luiscr918 <lc9107040@gmail.com>
2025-06-25 17:53:14 -0500

    formulario 1 listo

--- pages/Formulario1Screen.tsx
+++ pages/Formulario1Screen.tsx
@@ -1,9 +1,43 @@
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Alert, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
 import { TextInput } from "react-native-paper";
-
+import Entypo from '@expo/vector-icons/Entypo';
+interface Usuario {
+  nombre: string;
+  apellido: string;
+  email: string;
+  telefono: string;
+  contrasenia: string;
+}
 const Formulario1Screen = () => {
-  const [text, setText] = React.useState("");
+  //objeto con mis datos
+  const [usuarioPrueba, setUsuarioPrueba] = useState<Usuario>({
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: '',
+    contrasenia: ''
+  })
+  //conf contraseña
+  const [confContra, setConfContra] = useState("");
+  //guardar
+  const guardar = () => {
+    if (usuarioPrueba.nombre.trim() === '' || usuarioPrueba.apellido.trim() === ''
+      || usuarioPrueba.email.trim() === '' || usuarioPrueba.telefono.trim() === ''
+      || usuarioPrueba.contrasenia.trim() === '' || confContra.trim() == '') {
+      Alert.alert("Error", "Por favor llene todos los campos");
+      return;
+    }
+    if (confContra != usuarioPrueba.contrasenia) {
+      Alert.alert("Error", "Los campo de contraseña no coinciden");
+      return;
+    }
+    Alert.alert("Felicidades", "Te registraste correctamente: "
+      + usuarioPrueba.nombre.concat(" ", usuarioPrueba.apellido) + "\n"
+      + usuarioPrueba.email + "\n"
+      + usuarioPrueba.telefono
+    )
+  }
   return (
     <ImageBackground
       style={styles.container}
@@ -11,47 +45,89 @@ const Formulario1Screen = () => {
         uri: "https://4kwallpapers.com/images/walls/thumbs_3t/22691.jpg",
       }}
     >
-      <Text>Registro de Usuario</Text>
+      <Text style={styles.title}>Registro de Usuario</Text>
       <View style={styles.inputDoble}>
         <TextInput
-          style={styles.input}
+          style={{
+            ...styles.input,
+            width: '40%'
+          }}
           label="Nombre"
-          value={text}
-          onChangeText={(text) => setText(text)}
+          value={usuarioPrueba.nombre}
+          onChangeText={(text) => setUsuarioPrueba({
+            ...usuarioPrueba,
+            "nombre": text
+          })}
         />
         <TextInput
-          style={styles.input}
+          style={{
+            ...styles.input,
+            width: '40%'
+          }}
           label="Apellido"
-          value={text}
-          onChangeText={(text) => setText(text)}
+          value={usuarioPrueba.apellido}
+          onChangeText={(text) => setUsuarioPrueba({
+            ...usuarioPrueba,
+            "apellido": text
+          })}
         />
       </View>
       <View style={styles.inputDoble}>
         <TextInput
-          style={styles.input}
+          style={{
+            ...styles.input,
+            width: '40%'
+          }}
           label="Email"
-          value={text}
-          onChangeText={(text) => setText(text)}
+          value={usuarioPrueba.email}
+          onChangeText={(text) => setUsuarioPrueba({
+            ...usuarioPrueba,
+            "email": text
+          })}
         />
         <TextInput
-          style={styles.input}
+          style={{
+            ...styles.input,
+            width: '40%'
+          }}
           label="Teléfono"
-          value={text}
-          onChangeText={(text) => setText(text)}
+          value={usuarioPrueba.telefono}
+          onChangeText={(text) => {
+            if (/^\d{0,10}$/.test(text)) {
+              setUsuarioPrueba({
+                ...usuarioPrueba,
+                "telefono": text
+              })
+            } else {
+              Alert.alert("Solo se permiten numeros y solo numeros de 10 caracteres")
+              return;
+            }
+          }
+          }
         />
       </View>
       <TextInput
         style={styles.input}
         label="Contraseña"
-        value={text}
-        onChangeText={(text) => setText(text)}
+        value={usuarioPrueba.contrasenia}
+        onChangeText={(text) => setUsuarioPrueba({
+          ...usuarioPrueba,
+          "contrasenia": text
+        })}
       />
       <TextInput
         style={styles.input}
         label="Confirmar Contraseña"
-        value={text}
-        onChangeText={(text) => setText(text)}
+        value={confContra}
+        onChangeText={(text) => setConfContra(text)}
       />
+      <TouchableOpacity
+        onPress={guardar}
+        style={styles.btn}>
+        <Text style={{ fontSize: 30, color: '#ffffff' }}>Guardar</Text>
+        <Entypo name="save" size={35} color="white" style={{ margin: 4 }} />
+      </TouchableOpacity>
+
     </ImageBackground>
   );
 };
@@ -59,6 +135,19 @@ const Formulario1Screen = () => {
 export default Formulario1Screen;
 
 const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: '#e478ed',
+    flexDirection: 'row',
+    alignContent: 'space-around',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    width: '40%',
+    height: 60,
+    padding: 10,
+    marginTop: 15
+
+  },
   container: {
     flex: 1,
     alignItems: "center",
@@ -69,10 +158,16 @@ const styles = StyleSheet.create({
     width: "80%",
     height: 20,
     margin: 5,
-    
+
   },
-  inputDoble:{
-    backgroundColor:'#5dd05d',
-    width:'100%',
+  inputDoble: {
+    flexDirection: 'row',
+    padding: 4
+  },
+  title: {
+    fontSize: 40,
+    color: '#fdfcfc',
+    elevation: 5,
+    fontWeight: 'bold'
   }
 });
